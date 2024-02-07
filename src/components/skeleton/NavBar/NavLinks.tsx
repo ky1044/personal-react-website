cdff6292eed417d08020631a5d27250435fffd15
ky1044 <ky1044@nyu.edu>
2024-02-06 22:43:58 -0500

    add SiteDataProvider and display last updated time

--- src/components/skeleton/NavBar/NavLinks.tsx
+++ src/components/skeleton/NavBar/NavLinks.tsx
@@ -1,13 +1,36 @@
-import { Col, Divider, Row } from "antd";
-import { Link } from "react-router-dom";
-import styles from "./NavLinks.module.css";
-import { formatDate } from "src/utils/date";
+import { Row } from "antd";
 import { useMemo } from "react";
-import { MoonIcon, SunIcon } from "src/icons/Icons";
+import { Link } from "react-router-dom";
+import { useSiteData } from "src/providers/SiteDataProvider";
+import { formatDate } from "src/utils/date";
 import DarkModeToggle from "./DarkModeToggle";
+import styles from "./NavLinks.module.css";
 
 function NavBarLinks({ expanded }: { expanded: boolean }) {
-  const formattedDate = useMemo(() => formatDate(new Date()), []);
+  const { data, loading } = useSiteData();
+  const { latestDeploy } = data;
+  const formattedDate = useMemo(() => {
+    if (loading) {
+      return null;
+    }
+    const currentTime = new Date();
+
+    if (latestDeploy && latestDeploy > 0) {
+      const deployTime = new Date(latestDeploy);
+      const timeDiff = +currentTime - +deployTime;
+      const daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+      if (daysDiff <= 0) {
+        return "Last updated today";
+      } else if (daysDiff === 1) {
+        return "Last updated 1 day ago";
+      } else {
+        return `Last updated ${daysDiff} days ago`;
+      }
+    } else {
+      // fallback if we can't get latestDeploy, show current date
+      return formatDate(new Date());
+    }
+  }, [latestDeploy, loading]);
 
   return (
     <div
---
+++ src/providers/SiteDataProvider.tsx
@@ -0,0 +1,45 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+type SiteData = {
+  latestDeploy: number | null;
+};
+interface ISiteDataProvider {
+  data: SiteData;
+  loading: boolean;
+}
+const initialState = {
+  data: {
+    latestDeploy: null,
+  },
+  loading: true,
+};
+
+const SiteDataContext = createContext<ISiteDataProvider>(initialState);
+
+export const useSiteData = () => useContext(SiteDataContext);
+
+export const SiteDataProvider: React.FC<{
+  children: React.ReactNode;
+}> = ({ children }) => {
+  const [data, setData] = useState<ISiteDataProvider>(initialState);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const resp = await fetch(
+          "https://ky1044.github.io/personal-react-website/data/data.json"
+        );
+        const respJson = await resp.json();
+        setData({ data: respJson, loading: false });
+      } catch (e) {
+        console.log("error loading static data file\n", e);
+        setData((prev) => ({ ...prev, loading: false }));
+      }
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <SiteDataContext.Provider value={data}>{children}</SiteDataContext.Provider>
+  );
+};
